5a10cb7fb3648358a298854b59eac1d1d5e78a93
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-13 10:51:24 +0200

    ajouut de fichier test

--- tp1.php
+++ tp1.php
@@ -7,6 +7,10 @@
 
 <h1>This is a Heading</h1>
 <p>This is a paragraph.</p>
+<?php
+echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+echo 'C\'est pas mal non ? <br/>';
+?>
 
 </body>
 </html>
