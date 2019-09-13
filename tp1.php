231362facfb422a12ffba3d941516078b702518d
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-13 10:52:48 +0200

    ajouut de fichier test

--- tp1.php
+++ tp1.php
@@ -11,6 +11,11 @@
 echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
 echo 'C\'est pas mal non ? <br/>';
 ?>
+<?php
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
+?>
 
 </body>
 </html>
