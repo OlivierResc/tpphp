6e8d542344865c757cf4ce3992ada75f5f712350
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-13 11:07:23 +0200

    ajouut de fichier test

--- tp1.php
+++ tp1.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Page Title</title>
-</head>
-<body>
-
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
 <?php
-echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-echo 'C\'est pas mal non ? <br/>';
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
 ?>
-
-<?php
-$var1 = 6;
-$var2 = 1.3;
-$var3 = 'Variable 3';
-?>
-
-<?php echo "$var1 + $var2"; ?>
-<?php echo $var1 + $var2; ?>
-
-<?php echo $var1 + $var3; ?>
-
-
-
-</body>
-</html>
