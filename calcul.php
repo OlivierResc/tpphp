e64e083d2efa63ef691a4ac03f9f370e9a11869e
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-23 15:14:00 +0200

    ajouut de fichier calculator

---
+++ calcul.php
@@ -0,0 +1,43 @@
+<?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
+?>
+
+<?php
+function end_page()
+{
+    echo '</body></html>'
+    ;
+};
+?>
+
+<?php
+start_page('calculator');
+?>
+
+<?php
+$op1 = $_POST['op1'];
+$op2 = $_POST['op2'];
+$op = $_POST['op'];
+?>
+
+<?php
+if('*' == $op)
+{
+}
+elseif('+' == $op)
+{
+}
+else
+{
+    echo '<br/><strong>opérateur ' . $op . ' non géré </strong>';
+}
+?>
+
+<?php
+end_page();
+?>
