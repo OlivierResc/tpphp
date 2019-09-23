c4ae6302018f0c2f71b93edddc8a5d3b6a388039
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-23 15:08:23 +0200

    ajouut de fichier calculator

--- calculator.php
+++ calculator.php
@@ -22,8 +22,8 @@ start_page('calculator');
 
 
     <form action="calcul.php" method="post">
-        <input type="text" name="op"/>.'<br/>'
-        <input type="text" name="op"/>.'<br/>'
+        <input type="text" name="op"/><br/>
+        <input type="text" name="op"/><br/>
         <input checked="checked" type="radio" name="op" value="*"/>*<br/>
         <input type="radio" name="op" value="+"/>+<br/>
         <input type="radio" name="op" value="-"/>-<br/>
