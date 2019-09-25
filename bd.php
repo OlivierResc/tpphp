80aea0b0584762557ee73082adea35d143c9e901
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-25 17:28:38 +0200

    bd

--- bd.php
+++ bd.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbLink = mysqli_connect(mysql-olivier-rescigno-portofolio.alwaysdata.net, 174440, MagicOR14)
+$dbLink = mysqli_connect('mysql-olivier-rescigno-portofolio.alwaysdata.net', 174440, 'MagicOR14')
 or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 
 ?>
 
 <?php
-mysqli_select_db($dbLink , user)
+mysqli_select_db($dbLink , 'user')
 or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 );
 ?>
