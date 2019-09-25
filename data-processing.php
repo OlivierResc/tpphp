08492721c3b0e325f4623e7b4172ea068b880602
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-25 16:50:38 +0200

    formulaire

--- data-processing.php
+++ data-processing.php
@@ -3,8 +3,13 @@ include 'utils.inc.php';
 ?>
 
 <?php
-$email= $_POST ['e'];
+$tel= $_POST ['tel'];
+$id= $_POST ['id'];
+$pays= $_POST ['pays'];
+$email= $_POST ['email'];
 $password= $_POST ['mdp'];
+$Civilité= $_POST ['Civilité'];
+$Cond= $_POST ['Cond'];
 $action = $_POST['action'];
 
 ?>
@@ -14,7 +19,10 @@ if($action == 'mailer')
 {
 $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
 $message .= 'Email : ' . $email . PHP_EOL;
+$message .= 'identifiant : ' . PHP_EOL . $id;
 $message .= 'Mot de passe : ' . PHP_EOL . $password;
+$message .= 'Téléphone : ' . PHP_EOL . $tel;
+$message .= 'Pays : ' . PHP_EOL . $pays;
 mail($email,'inscription', $message);
 echo '<br/><strong>Mail envoyé gros bg!</strong><br/>';
 }
--- td2.php
+++ td2.php
@@ -7,16 +7,16 @@ include 'utils.inc.php';
     <input type="text" name="id" placeholder="Identifiant"/> <br>
     <input type="radio" value="Homme" name="Civilité" checked/> Homme <br>
     <input type="radio" value="Femme" name="Civilité" checked/> Femme <br>
-    <input type="text" name="e" placeholder="E-mail"/> <br>
+    <input type="text" name="email" placeholder="E-mail"/> <br>
     <input type="password" name="mdp" placeholder="Mot de passe"/> <br>
     <input type="password" placeholder="Verif mdp"/> <br>
     <input type="text" name="tel" placeholder="Téléphone"/> <br>
-    <select>
+    <select name="pays">
         <option> Coatie </option>
         <option> Serbie </option>
         <option> France </option>
         <option> Japon </option>
     </select> <br>
-    <input type="checkbox" value="Conditions d'utilisations"/> Conditions Générales <br>
+    <input type="checkbox" name="Cond" value="Conditions d'utilisations"/> Conditions Générales <br>
     <input type="submit" name="action" value="mailer" placeholder="Bouton de soumission"/> <br>
 </form>
