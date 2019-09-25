fea1e9598ca31962ef7232ebffab713fed5b2bcd
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-25 17:25:53 +0200

    bd

--- bd.php
+++ bd.php
@@ -1,6 +1,12 @@
 <?php
 
-$dbLink = mysqli_connect(localhost, 174440, MagicOR14***)
+$dbLink = mysqli_connect(localhost, 174440, MagicOR14)
 or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 
+?>
+
+<?php
+mysqli_select_db($dbLink , user)
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
+);
 ?>
