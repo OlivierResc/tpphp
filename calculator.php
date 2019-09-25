c75e2dab3c4f86b42bf61fd909f0eb09c9845ce3
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-25 14:13:14 +0200

    ajouut de fichier calculator

--- calculator.php
+++ calculator.php
@@ -28,17 +28,17 @@ $operateurs = '*+-/';
         <input type="text" name="op1"/><br/>
         <input type="text" name="op2"/><br/>
 
-    <?php
-        for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
-        {
-        echo '<input ';
-        if($cpt == 0)
-        {
-        echo 'checked="checked" ';
-        }
-        echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
-        }
-     ?>
+<!--    --><?php
+//        for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
+//        {
+//        echo '<input ';
+//        if($cpt == 0)
+//        {
+//        echo 'checked="checked" ';
+//        }
+//        echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
+//        }
+//     ?>
         <input type="submit" name="action" value="*"/>
         <input type="submit" name="action" value="+"/>
         <input type="submit" name="action" value="-"/>
