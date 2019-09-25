454b9c7ad645bac5c111d2ec9e9f5e13dd3f53b3
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-25 15:17:47 +0200

    formulaire

--- td2.php
+++ td2.php
@@ -1,16 +1,16 @@
 <form action="" method="post">
     <input type="text" placeholder="Identifiant"/> <br>
-    <input type="radio" value="Civilité"/> <br>
-    <input type="text"value="E-mail"/> <br>
-    <input type="password"value="Mot de passe"/> <br>
-    <input type="password"value="Verif mdp"/> <br>
-    <input type="text"value="Téléphone"/> <br>
+    <input type="radio" placeholder="Civilité"/> <br>
+    <input type="text" placeholder="E-mail"/> <br>
+    <input type="password" placeholder="Mot de passe"/> <br>
+    <input type="password" placeholder="Verif mdp"/> <br>
+    <input type="text" placeholder="Téléphone"/> <br>
     <select>
         <option> Coatie </option>
         <option> Serbie </option>
         <option> France </option>
         <option> Japon </option>
     </select> <br>
-    <input type="checkbox"value="Conditions d'utilisations"/> <br>
-    <input type="submit"value="Bouton de soumission"/> <br>
+    <input type="checkbox" placeholder="Conditions d'utilisations"/> <br>
+    <input type="submit" placeholder="Bouton de soumission"/> <br>
 </form>
