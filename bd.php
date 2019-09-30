266930b5c919126cd7e1c582e4fd79021d69ff2d
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-30 08:37:39 +0200

    bd

--- bd.php
+++ bd.php
@@ -10,3 +10,31 @@ mysqli_select_db($dbLink , 'olivier-rescigno-portofolio_bd')
 or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 );
 ?>
+
+<?php
+$query = 'SELECT id, email, date FROM user';
+?>
+
+<?php
+if(!($dbResult = mysqli_query($dbLink, $query)))
+{
+    echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+?>
+
+<?php
+while($dbRow = mysqli_fetch_assoc($dbResult))
+{
+
+    echo $dbRow['id'] . '<br/>';
+    echo $dbRow['email'] . '<br/>';
+    echo $dbRow['date'] . '<br/>';
+    echo '<br/><br/>';
+
+}
+?>
