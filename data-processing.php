ae5f4d6ed86dfcc6f05e3643ee9d5ac4758f6f7c
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-30 10:13:06 +0200

    connexion bd formulaire

--- data-processing.php
+++ data-processing.php
@@ -56,7 +56,7 @@ $query = 'INSERT INTO user (email, mdp, tel, pays, Cond, Civilité, Identifiant,
          \'' . $tel . '\' ,
          \'' . $pays . '\' ,
          \'' . $Cond . '\' ,
-         \'' . $Civilite . '\',
+         \'' . 'homme' . '\',
          \'' . $id .'\',
          \'' . $today .'\'
          )';
