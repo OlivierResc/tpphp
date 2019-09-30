9ab531a8353505791812d667a19ec263722a3495
r18000204 <olivier.rescigno@etu.univ-amu.fr>
2019-09-30 09:46:26 +0200

    bd

--- data-processing.php
+++ data-processing.php
@@ -50,14 +50,14 @@ $today = date('Y-m-d');
 ?>
 
 <?php
-$query = 'INSERT INTO user (date, email , id, mdp, tel, pays, Cond, Civilité) VALUES (\'' . $today . '\' , \''
-    . $email . '\', , \''
-    . $id . '\', , \''
-    . $password . '\', , \''
-    . $pays . '\', , \''
-    . $tel . '\', , \''
-    . $Cond . '\', , \''
-    . $Civilité .'\' . )';
+$query = 'INSERT INTO user (date, email , id, mdp, tel, pays, Cond, Civilité) VALUES (\'' . $today . '\' ,
+    \''. $email . '\',
+    \''. $id . '\',
+    \''. $password . '\',
+    \''. $pays . '\', 
+    \''. $tel . '\',  
+    \''. $Cond . '\',  
+    \''. $Civilité .'\' )';
 ?>
 
 <?php
